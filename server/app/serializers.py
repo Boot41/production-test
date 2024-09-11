5b15e301ae55deeb0b80c5c9688eca4ab23dedbe
root <root@localhost>
2024-09-11 09:43:16 +0000

    US-1-BT-2--Develop APIs to handle job listing updates and deletions. Employers should be able to modify existing listings and remove outdated ones.

--- server/app/serializers.py
+++ server/app/serializers.py
@@ -1,7 +1,1 @@
-from rest_framework import serializers
-from .models import Job
-
-class JobSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Job
-        fields = '__all__'
+from rest_framework import serializers\nfrom .models import Job\n\nclass JobSerializer(serializers.ModelSerializer):\n    class Meta:\n        model = Job\n        fields = '__all__'
